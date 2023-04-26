8771d6d4fbfe3e68cd010dc874cb38e0b327e92a
Renu <renu.rk166@gmail.com>
2023-04-26 16:58:31 +0530

    Count numers

--- src/components/practice-es6.js
+++ src/components/practice-es6.js
@@ -1,5 +1,14 @@
 let myLoop = ['rinku', 'renu', 'swayam', 'bravo'];
 
+
+
+// let a = [1,3,2,1,5,7,7,4,1,9,4,3,5,1]; select the unque numbers.
+
+
+// 
+
+
+
 myLoop.forEach(function(renu, index, array){
     console.log(renu)
 });
